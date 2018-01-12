a402f741d861047e236cf3de6e609ee547913cda
wgfiou <35366778+wgfiou@users.noreply.github.com>
2018-01-12 12:17:24 +0200

    Add files via upload

---
+++ HelloWorld/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Vesa Vuorio on 10/01/2018.
+//  Copyright © 2018 Vesa Vuorio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
